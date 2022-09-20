720e609b8f91b7f41a00657459a36d80fda30ed1
LightningNight1 <zoebennnettt@gmail.com>
2022-09-20 23:50:08 +0300

    task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter two numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Number {number1} is greater than {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Number {number2} is greater than {number1}");
+}
+else
+{
+    Console.WriteLine("The numbers are equal");
+}
