8beebc1ef0e38bab40ed5c7e12c4e36431ca4530
LightningNight1 <zoebennnettt@gmail.com>
2022-09-20 23:58:05 +0300

    task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    Console.WriteLine(i + " ");
+    i += 2;
+}
+if (N <= 1)
+{
+    Console.WriteLine("Invalid value");
+}
